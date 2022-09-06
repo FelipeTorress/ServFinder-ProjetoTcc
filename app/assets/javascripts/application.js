fe305208272782d0df4d0c75cbf276d23614831c
Felipe Torres <felipetoress@gmail.com>
2022-09-06 02:11:59 -0300

    feat: mapas na aplicacao

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,28 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+function chamarMapaLocAproximada(){
+    var map = L.map('map').setView([51.505, -0.09], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  
+    L.circle([51.505, -0.09], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(map)
+}
+
+function chamarMapaLocExata(){
+    var map = L.map('map').setView([51.505, -0.09], 14);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  
+    L.marker([51.5, -0.09]).addTo(map);
+}
