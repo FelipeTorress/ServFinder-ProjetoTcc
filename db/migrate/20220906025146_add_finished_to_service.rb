32f5d66f4732cea75ff9ce85d3e15f9a049baf3c
Felipe Torres <felipetoress@gmail.com>
2022-09-28 22:23:44 -0300

    refactor: movendo declaracao de index para dentro da funcao change

--- db/migrate/20220906025146_add_finished_to_service.rb
+++ db/migrate/20220906025146_add_finished_to_service.rb
@@ -1,6 +1,6 @@
 class AddFinishedToService < ActiveRecord::Migration[5.2]
   def change
     add_column :services, :finished, :boolean, default: false
+    add_index :services, :finished
   end
-  add_index :services, :finished
 end
--- db/migrate/20220919234008_add_service_finished_prestador.rb
+++ db/migrate/20220919234008_add_service_finished_prestador.rb
@@ -1,6 +1,6 @@
 class AddServiceFinishedPrestador < ActiveRecord::Migration[5.2]
   def change
     add_column :services, :finished_prestador, :boolean, default: false
+    add_index :services, :finished_prestador
   end
-  add_index :services, :finished_prestador
 end
