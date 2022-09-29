aa3e768df5c43edc1bc7492b69915d8107641785
Felipe Torres <felipetoress@gmail.com>
2022-09-28 21:39:14 -0300

    correcao na criacao de index de user_selected_id

--- db/migrate/20220828171234_add_acept_cookies.rb
+++ db/migrate/20220828171234_add_acept_cookies.rb
@@ -2,5 +2,5 @@ class AddAceptCookies < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :accepted_cookies, :boolean, default: false
   end
-  add_index :users, :user_selected_id
+  add_index :services, :user_selected_id
 end
