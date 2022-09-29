14f7b2009cb2a632cb7ca26401b1242125452cf0
Felipe Torres <felipetoress@gmail.com>
2022-09-28 21:49:37 -0300

    refactor: removendo index duplicado

--- db/migrate/20220828171234_add_acept_cookies.rb
+++ db/migrate/20220828171234_add_acept_cookies.rb
@@ -2,5 +2,4 @@ class AddAceptCookies < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :accepted_cookies, :boolean, default: false
   end
-  add_index :services, :user_selected_id
 end
